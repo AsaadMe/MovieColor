61288c5631a89e056f76b9b180c5b01bab15607d
Mehran Asaad <mehran.asaad@gmail.com>
2021-03-11 20:26:24 +0330

    create the main class to transfer functionalities to its instances

--- moviecolor/__main__.py
+++ moviecolor/__main__.py
@@ -1,8 +1,10 @@
-from moviecolor.moviecolor import *
+#from moviecolor import *
+from moviecolor.moviecolor import movcolor
 from pathlib import Path
 import tkinter as tk
 import argparse
 import threading
+#import multiprocessing
 import time
 
 parser = argparse.ArgumentParser()
@@ -12,7 +14,7 @@ parser.add_argument('-l','--length', type=int, default=0 , help='Chosen part of 
 parser.add_argument('-a','--alt', action='store_true', help='Instead of gettig average color of frames, Each bar is the resized frame')
 
 def main():
-    root = tk.Tk()
+
     args = parser.parse_args()
 
     input_file_path = args.in_file
@@ -27,29 +29,41 @@ def main():
 
     output_file_path = args.out_filename
     video_length = args.length
+
+    obj1 = movcolor(1, input_file_path, output_file_path)
+
     if video_length != 0:
         number_of_frames = video_length * 60 * 3
     else:
-        duration = get_video_duration(input_file_path)
+        duration = obj1.get_video_duration()
         number_of_frames = duration * 3
         video_length = int(duration/60)
-
+    
     if args.alt:
-        process_func = process_frame_compress_width
-        refresh_image = refresh_image_alt
-        draw_func = draw_alt
+        process_func = movcolor.process_frame_compress_width
+        refresh_image = obj1.refresh_image_alt
+        draw_func = obj1.draw_alt
     else:
-        process_func = process_frame_average_color
-        refresh_image = refresh_image_normal
-        draw_func = draw_normal
+        process_func = movcolor.process_frame_average_color
+        refresh_image = obj1.refresh_image_normal
+        draw_func = obj1.draw_normal
 
-    th = threading.Thread(target=run, args=(input_file_path, output_file_path, video_length ,process_func, draw_func))
+    obj2 = movcolor(2,input_file_path, output_file_path.parent / "res2")
+    th = threading.Thread(target=obj1, args=(video_length ,process_func, draw_func, 0))
+    #th2 = threading.Thread(target=obj2, args=(10 ,process_func, obj2.draw_normal, 5))
+
+    #th = multiprocessing.Process(target=obj1, args=(video_length ,process_func, draw_func, 0))
+    #th2 = multiprocessing.Process(target=obj2, args=(10 ,process_func, obj2.draw_normal, 5))
     th.daemon = True  # terminates whenever main thread does
     th.start()
-    while len(rgb_list) == 0:  # rgb_list in refresh_image shouldnt be empty
+    #th2.start()
+    
+    while len(obj1.rgb_list) == 0:  # rgb_list in refresh_image shouldnt be empty
         time.sleep(.1)
 
+    root = tk.Tk()
     canvas = tk.Canvas(root, height=720, width=1500)
+
     root.title("MovieColor")
     root.geometry("1500x720+0+10")
     canvas.pack()
--- moviecolor/moviecolor.py
+++ moviecolor/moviecolor.py
@@ -4,155 +4,166 @@ import logging
 import numpy as np
 import subprocess
 
+class movcolor:
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
 
-rgb_list = []
-bars_flag = 0
+    def __call__(self, *args):
+        self.run(*args)
 
-def get_video_duration(filepath):
-    probe = ffmpeg.probe(filepath)
-    video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
-    duration = int(video_info['duration'].split('.')[0])
-    return duration
+    def __init__(self, id, in_path, out_path) -> None:
+        self.id = id
+        self.in_path = in_path
+        self.out_path = out_path
+        self.bars_flag = 0
+        self.rgb_list = []
 
-def get_video_size(filepath):
-    logger.info('Getting video size for {!r}'.format(filepath))
-    probe = ffmpeg.probe(filepath)
-    video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
-    width = int(video_info['width'])
-    height = int(video_info['height'])
-    return width, height
+    def get_video_duration(self):
+        probe = ffmpeg.probe(self.in_path)
+        video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
+        duration = int(video_info['duration'].split('.')[0])
+        return duration
 
-
-def start_ffmpeg_process1(in_filename, length):
-    logger.info('Starting ffmpeg process1')
-    args = (
-        ffmpeg
-        .input(in_filename)
-        .trim(end=length*60)
-        .filter_('fps',fps=3) # get 3 frames per second
-        .output('pipe:', format='rawvideo', pix_fmt='rgb24')
-        .compile()
-    )
-    return subprocess.Popen(args, stdout=subprocess.PIPE)
+    def get_video_size(self):
+        self.logger.info('Getting video size for {!r}'.format(self.in_path))
+        probe = ffmpeg.probe(self.in_path)
+        video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
+        width = int(video_info['width'])
+        height = int(video_info['height'])
+        return width, height
 
 
-def read_frame(process1, width, height):
-    logger.debug('Reading frame')
-
-    # Note: RGB24 == 3 bytes per pixel.
-    frame_size = width * height * 3
-    in_bytes = process1.stdout.read(frame_size)
-    if len(in_bytes) == 0:
-        frame = None
-    else:
-        assert len(in_bytes) == frame_size
-        frame = (
-            np
-            .frombuffer(in_bytes, np.uint8)
-            .reshape([height, width, 3])
+    def start_ffmpeg_process1(self, start, length):
+        self.logger.info('Starting ffmpeg process1')
+        args = (
+            ffmpeg
+            .input(self.in_path)
+            .trim(start=start*60, end=length*60)
+            .filter_('fps',fps=3) # get 3 frames per second
+            .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+            .compile()
         )
-    return frame
+        return subprocess.Popen(args, stdout=subprocess.PIPE)
 
 
-def process_frame_average_color(frame):   
-    rgb_avg = int(np.average(frame[:,:,0])),int(np.average(frame[:,:,1])),int(np.average(frame[:,:,2]))
-    return rgb_avg
+    def read_frame(self, process1, width, height):
+        self.logger.debug('Reading frame')
 
-def process_frame_compress_width(frame):
-    img = Image.fromarray(frame, 'RGB').resize((1,720))
-    return img
+        # Note: RGB24 == 3 bytes per pixel.
+        frame_size = width * height * 3
+        in_bytes = process1.stdout.read(frame_size)
+        if len(in_bytes) == 0:
+            frame = None
+        else:
+            assert len(in_bytes) == frame_size
+            frame = (
+                np
+                .frombuffer(in_bytes, np.uint8)
+                .reshape([height, width, 3])
+            )
+        return frame
 
-def draw_alt(rgb_list, out_path):
-    len_rgb_list = len(rgb_list)
+    @staticmethod
+    def process_frame_average_color(frame):   
+        rgb_avg = int(np.average(frame[:,:,0])),int(np.average(frame[:,:,1])),int(np.average(frame[:,:,2]))
+        return rgb_avg
     
-    image_height = int(len_rgb_list*9/16) # to make a 16:9
-    new = Image.new('RGB',(int(len_rgb_list/2),image_height))
-    for i in range(len_rgb_list-1):      
-        new.paste(rgb_list[i], (int(i/2), 0))
+    @staticmethod
+    def process_frame_compress_width(frame):
+        img = Image.fromarray(frame, 'RGB').resize((1,720))
+        return img
 
-    if out_path.suffix.lower() == ".jpg":
-        suff = "JPEG"
-    elif out_path.suffix.lower() == ".png":
-        suff = "PNG"
-    else:
-        suff = "PNG"
-        out_path = str(out_path) + ".png"
+    def draw_alt(self):
+        len_rgb_list = len(self.rgb_list)
+        
+        image_height = int(len_rgb_list*9/16) # to make a 16:9
+        new = Image.new('RGB',(len_rgb_list,image_height))
+        for i in range(len_rgb_list-1):      
+            new.paste(self.rgb_list[i].resize((1,image_height)), (i, 0))
 
-    new.save(out_path, suff)
+        if self.out_path.suffix.lower() == ".jpg":
+            suff = "JPEG"
+        elif self.out_path.suffix.lower() == ".png":
+            suff = "PNG"
+        else:
+            suff = "PNG"
+            self.out_path = str(self.out_path) + ".png"
 
-def draw_normal(rgb_list, out_path):
-    len_rgb_list = len(rgb_list)
-    
-    image_height = int(len_rgb_list*9/16) # to make a 16:9
-    new = Image.new('RGB',(int(len_rgb_list),image_height))
-    draw = ImageDraw.Draw(new)
-    x_pixel = 1 # x axis of the next line to draw
-    for rgb_tuple in rgb_list:
-        draw.line((x_pixel,0,x_pixel,image_height), fill=rgb_tuple)
-        x_pixel = x_pixel + 1
-    
-    if out_path.suffix.lower() == ".jpg":
-        suff = "JPEG"
-    elif out_path.suffix.lower() == ".png":
-        suff = "PNG"
-    else:
-        suff = "PNG"
-        out_path = str(out_path) + ".png"
+        new.save(self.out_path, suff)
 
-    new.save(out_path, suff)
+    def draw_normal(self):
+        len_rgb_list = len(self.rgb_list)
+        
+        image_height = int(len_rgb_list*9/16) # to make a 16:9
+        new = Image.new('RGB',(int(len_rgb_list),image_height))
+        draw = ImageDraw.Draw(new)
+        x_pixel = 1 # x axis of the next line to draw
+        for rgb_tuple in self.rgb_list:
+            draw.line((x_pixel,0,x_pixel,image_height), fill=rgb_tuple)
+            x_pixel = x_pixel + 1
+        
+        if self.out_path.suffix.lower() == ".jpg":
+            suff = "JPEG"
+        elif self.out_path.suffix.lower() == ".png":
+            suff = "PNG"
+        else:
+            suff = "PNG"
+            self.out_path = str(self.out_path) + ".png"
+
+        new.save(self.out_path, suff)
 
 
-def run(in_path, out_filename, length, process_frame, draw_func):
-    
-    width, height = get_video_size(in_path)
-    process1 = start_ffmpeg_process1(in_path, length)
-    while True:
-        in_frame = read_frame(process1, width, height)
-        if in_frame is None:
-            logger.info('End of input stream')
-            break
+    def run(self, length, process_frame, draw_func, start):
+        
+        width, height = self.get_video_size()
+        process1 = self.start_ffmpeg_process1(start, length)
 
-        logger.debug('Processing frame')
-        out_frame = process_frame(in_frame)
-        global rgb_list      
-        rgb_list.append(out_frame)
+        while True:
+            in_frame = self.read_frame(process1, width, height)
+            if in_frame is None:
+                self.logger.info('End of input stream')
+                break
 
-    global bars_flag
-    bars_flag = len(rgb_list)
-    draw_func(rgb_list, out_filename)
-      
-    logger.info('Waiting for ffmpeg process1')
-    process1.wait()
+            self.logger.debug('Processing frame')
+            out_frame = process_frame(in_frame)
+                 
+            self.rgb_list.append(out_frame)
 
-    logger.info('Done')
 
-def refresh_image_alt(canvas, x_pixel, number_of_frames, *param):
-    
-    dst = Image.new('RGB', (1500, 720))
+        self.bars_flag = len(self.rgb_list)
+        draw_func()
 
-    if len(param) != 0 : 
-        dst = param[0]
+        
+        self.logger.info('Waiting for ffmpeg process1')
+        process1.wait()
 
-    step = 1500 / number_of_frames
-    for rgb_tuple in rgb_list[int((x_pixel-1)*(1/step)):]: 
-        dst.paste(rgb_tuple, (int(x_pixel), 0))
-        x_pixel += step
-    global image
-    image = ImageTk.PhotoImage(dst)
-    canvas.create_image((750, 360), image=image)
+        self.logger.info('Done')
 
-    if len(rgb_list) != bars_flag:
-        canvas.after(100, refresh_image_alt, canvas, x_pixel, number_of_frames, dst)
+    def refresh_image_alt(self, canvas, x_pixel, number_of_frames, *param):
+        
+        dst = Image.new('RGB', (1500, 720))
 
-def refresh_image_normal(canvas, x_pixel, number_of_frames):        
-    image_height = 720
-    step = 1500 / number_of_frames
-    for rgb_tuple in rgb_list[int((x_pixel-1)*(1/step)):]:
-        canvas.create_line((x_pixel,0,x_pixel,image_height), fill='#%02x%02x%02x' % rgb_tuple, width=step)
-        x_pixel = x_pixel + step
+        if len(param) != 0 : 
+            dst = param[0]
 
-    if len(rgb_list) != bars_flag:
-        canvas.after(100, refresh_image_normal, canvas, x_pixel-step, number_of_frames)
+        step = 1500 / number_of_frames
+        for rgb_tuple in self.rgb_list[int((x_pixel-1)*(1/step)):]: 
+            dst.paste(rgb_tuple, (int(x_pixel), 0))
+            x_pixel += 1
+        global image
+        image = ImageTk.PhotoImage(dst)
+        canvas.create_image((750, 360), image=image)
+
+        if len(self.rgb_list) != self.bars_flag:
+            canvas.after(100, self.refresh_image_alt, canvas, x_pixel, number_of_frames, dst)
+
+    def refresh_image_normal(self, canvas, x_pixel, number_of_frames):        
+        image_height = 720
+        step = 1500 / number_of_frames
+        for rgb_tuple in self.rgb_list[int((x_pixel-1)*(1/step)):]:
+            canvas.create_line((x_pixel,0,x_pixel,image_height), fill='#%02x%02x%02x' % rgb_tuple, width=step)
+            x_pixel += 1
+
+        if len(self.rgb_list) != self.bars_flag:
+            canvas.after(100, self.refresh_image_normal, canvas, x_pixel-step, number_of_frames)
